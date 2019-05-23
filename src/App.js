85cac04eba9bb8ef1f9b953032c0557819aaef80
Geoff-Hignett <geoffhig@hotmail.com>
2019-05-23 13:23:57 +0100

    Routes updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import AddFestival from './components/festivals/AddFestival';
 import Header from './components/layout/Header';
 import About from './components/pages/About';
 import NotFound from './components/pages/NotFound';
-import Test from './components/test/Test';
 
 import { Provider } from './context';
 
@@ -24,7 +23,6 @@ class App extends Component {
                 <Route exact path="/" component={Festivals} />
                 <Route exact path="/festival/add" component={AddFestival} />
                 <Route exact path="/about/" component={About} />
-                <Route exact path="/test/" component={Test} />
                 <Route component={NotFound} />
               </Switch>
             </div>
